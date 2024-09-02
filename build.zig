88c3d44f31c3aa19f567af3256987fe319ed75e4
Johan Rimez <96076981+JohanRimez@users.noreply.github.com>
2024-09-02 14:03:36 +0200

    Typo correction in build.zig

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "ZigRipples",
+        .name = "ZigBlobs",
         .root_source_file = b.path("src/ZigBlobs.zig"),
         .target = target,
         .optimize = optimize,
